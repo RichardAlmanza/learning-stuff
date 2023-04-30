e6d1a38124e76af803a2a54ec49a81b54ee0ed24
Richard Almanza <al.vegari@gmail.com>
2023-04-30 07:14:13 +0000

    use props for Testimonial

--- react/testimonials-freecodecamp/src/components/Testimonial/Testimonial.jsx
+++ react/testimonials-freecodecamp/src/components/Testimonial/Testimonial.jsx
@@ -1,24 +1,31 @@
+import PropTypes from "prop-types";
 import "./Testimonial.css";
 
 const EmmaImage = require("../../images/testimonial-emma.png");
 
-function Testimonial() {
+function Testimonial({ name, role, country, company, testimonial }) {
   return (
     <div className="testimonial-container">
       <img className="testimonial-image" src={EmmaImage} alt="emma" />
       <div className="testimonial-text-container">
-        <p className="testimonial-name">Emma Bostian in Sweden</p>
-        <p className="testimonial-role">Software Engineer at Spotify</p>
-        <p className="testimonial-text">
-          &quot;I&apos;ve always struggled with learning JavaScript. I&apos;ve
-          taken many courses but freeCodeCamp&apos;s course was the one which
-          stuck. Studying JavaScript as well as data structures and algorithms
-          on freeCodeCamp gave me the skills and confidence I needed to land my
-          dream job as a software engineer at Spotify.&quot;
+        <p className="testimonial-name">
+          {name} in {country}
         </p>
+        <p className="testimonial-role">
+          {role} at {company}
+        </p>
+        <p className="testimonial-text">{testimonial}</p>
       </div>
     </div>
   );
 }
+
+Testimonial.propTypes = {
+  name: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+  testimonial: PropTypes.string.isRequired,
+};
 
 export default Testimonial;
