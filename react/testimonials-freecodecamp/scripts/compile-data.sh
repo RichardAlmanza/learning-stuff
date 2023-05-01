521ab8eafea5530deb2c07b7448d53a688d6cc2b
Richard Almanza <al.vegari@gmail.com>
2023-05-01 05:04:33 +0000

    add script to merge yaml files in src/assets/data into a json

---
+++ react/testimonials-freecodecamp/scripts/compile-data.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+# Using yarn or npm, the env variable PWD is set to the project's root path
+data_path=$PWD/src/assets/data
+
+files=$(find $data_path -type f \( -iname "*.yaml" -o -iname "*.yml" \))
+
+# check https://mikefarah.gitbook.io/yq/operators/multiply-merge to choose how to merge
+# *d -> Deep Merge, the order is important
+yq eval-all '. as $item ireduce ({}; . *d $item )' \
+    $files --colors --output-format=json \
+    | tee $data_path/data.json
