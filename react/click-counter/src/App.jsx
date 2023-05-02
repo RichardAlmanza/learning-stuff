a5e20db4e9a691f53c87c5bd16edce7b6ae7040e
Richard Almanza <al.vegari@gmail.com>
2023-05-02 06:31:31 +0000

    implement the addClick and resetCounter functions

--- react/click-counter/src/App.jsx
+++ react/click-counter/src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import reactLogo from "./assets/logo.svg";
 import Button from "./components/Button/Button";
 import Counter from "./components/Counter/Counter";
 import "./App.css";
 
 function App() {
+  const [nClick, setNClick] = useState(0);
+
   const addClick = () => {
-    console.log("click");
+    setNClick((clicks) => clicks + 1);
   };
 
   const resetCounter = () => {
-    console.log("reset");
+    setNClick(0);
   };
 
   return (
@@ -18,7 +21,7 @@ function App() {
         <img src={reactLogo} className="React-logo" alt="logo" />
       </div>
       <div className="click-counter-container">
-        <Counter clickCounter={5} />
+        <Counter clickCounter={nClick} />
         <Button text="Click" eventHandler={addClick} />
         <Button
           text="Reset"
