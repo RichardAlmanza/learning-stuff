278db882dc76a9c3613403e1dd7b789b0e1f9766
Richard Almanza <al.vegari@gmail.com>
2023-05-02 05:48:29 +0000

    add Button component

--- react/click-counter/src/App.jsx
+++ react/click-counter/src/App.jsx
@@ -1,13 +1,29 @@
 import reactLogo from "./assets/logo.svg";
+import Button from "./components/Button/Button";
 import "./App.css";
 
 function App() {
+  const addClick = () => {
+    console.log("click");
+  };
+
+  const resetCounter = () => {
+    console.log("reset");
+  };
+
   return (
     <div className="App">
       <div className="React-logo-container">
         <img src={reactLogo} className="React-logo" alt="logo" />
       </div>
-      <div className="click-counter-container" />
+      <div className="click-counter-container">
+        <Button text="Click" eventHandler={addClick} />
+        <Button
+          text="Reset"
+          isClickButton={false}
+          eventHandler={resetCounter}
+        />
+      </div>
     </div>
   );
 }
---
+++ react/click-counter/src/components/Button/Button.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+function Button({ text, eventHandler, isClickButton }) {
+  return (
+    <button
+      type="button"
+      className={isClickButton ? "click-button" : "reset-button"}
+      onClick={eventHandler}
+    >
+      {text}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  eventHandler: PropTypes.func.isRequired,
+  isClickButton: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  isClickButton: true,
+};
+
+export default Button;
