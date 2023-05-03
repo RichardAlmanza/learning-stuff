3ab1ed548b17ea7b418e9412e3783e4a6437db4d
Richard Almanza <al.vegari@gmail.com>
2023-05-03 00:44:42 +0000

    add Button to Calculator

--- react/calculator/src/components/Calculator/Calculator.jsx
+++ react/calculator/src/components/Calculator/Calculator.jsx
@@ -1,16 +1,33 @@
 import "./Calculator.css";
+import Button from "../Button/Button";
 
 function Calculator() {
   return (
     <div className="calculator-container">
-      <div className="row" />
-      <div className="row" />
-      <div className="row" />
-      <div className="row" />
-      <div className="row" />
-      <div className="row" />
-      <div className="row" />
-      <div className="row" />
+      <div className="row">
+        <Button value={1} />
+        <Button value={2} />
+        <Button value={3} />
+        <Button value="+" />
+      </div>
+      <div className="row">
+        <Button value={4} />
+        <Button value={5} />
+        <Button value={6} />
+        <Button value="-" />
+      </div>
+      <div className="row">
+        <Button value={7} />
+        <Button value={8} />
+        <Button value={9} />
+        <Button value="*" />
+      </div>
+      <div className="row">
+        <Button value="=" />
+        <Button value={0} />
+        <Button value="." />
+        <Button value="/" />
+      </div>
     </div>
   );
 }
