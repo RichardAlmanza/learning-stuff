58fd1f7ea6df99f4368421127cd7636ceee6471d
Richard Almanza <al.vegari@gmail.com>
2024-04-08 03:40:17 -0500

    go/snippetbox customize filesystem to Disable FileServer Directory Listings

--- go/snippetbox/cmd/web/main.go
+++ go/snippetbox/cmd/web/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	fileserver := http.FileServer(http.Dir("./ui/static/"))
-
+	fileserver := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
 	mux.HandleFunc("/", home)
@@ -19,4 +20,31 @@ func main() {
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+    fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	file, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	stats, _ := file.Stat()
+	if stats.IsDir() {
+		index := filepath.Join(path, "index.html")
+		_, err := nfs.fs.Open(index)
+		if err != nil {
+			closeErr := file.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return file, nil
 }
