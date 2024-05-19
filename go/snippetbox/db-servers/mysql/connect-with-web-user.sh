09c607bc14b6b61d8fe644cb7385017b285a7605
Richard Almanza <al.vegari@gmail.com>
2024-05-18 23:20:29 -0500

    go/snippetbox Setting up MySQL

---
+++ go/snippetbox/db-servers/mysql/connect-with-web-user.sh
@@ -0,0 +1,1 @@
+docker exec -it mysql-db-1 sh -c 'exec mysql -Dsnippetbox -uweb -pweb_password'
---
+++ go/snippetbox/db-servers/mysql/load-db.sh
@@ -0,0 +1,1 @@
+docker exec -i mysql-db-1 sh -c 'exec mysql -uroot -proot_password_89' < ./define-db.sql
