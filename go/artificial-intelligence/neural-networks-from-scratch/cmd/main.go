6c8deaa15915e93967401a3ae39f74cfc1679a27
Richard Almanza <al.vegari@gmail.com>
2025-12-08 01:03:12 -0500

    go/nnfs: moved single neuron definition to a new package

--- go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
+++ go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
@@ -1,56 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"math/rand"
+	"github.com/RichardAlmanza/learning-stuff/go/artificial-intelligence/neural-networks-from-scratch/pkgs/single-neuron"
 )
-
-type NeuronFloat64 struct {
-	InputSize int
-	Bias float64
-	Weights []float64
-}
-
-func NewNeuron(numInputs int) *NeuronFloat64 {
-	return &NeuronFloat64{
-		InputSize: numInputs,
-		Bias: 0,
-		Weights: make([]float64, numInputs),
-	}
-}
-
-func (n *NeuronFloat64) SetWeights(newWeights []float64) error {
-	if len(newWeights) != n.InputSize {
-		return errors.New("size mismatch")
-	}
-
-	n.Weights = newWeights
-
-	return nil
-}
-
-func (n *NeuronFloat64) RandomizeWeights() {
-	for i := 0; i < n.InputSize; i++ {
-		n.Weights[i] = (rand.Float64() - 0.5) * 10
-	}
-}
-
-func (n *NeuronFloat64) Synapsis(input []float64) (float64, error) {
-	if len(input) != n.InputSize {
-		return 0, errors.New("size mismatch")
-	}
-
-	var output float64 = 0
-
-	for i := 0; i < len(input); i++ {
-		output += n.Weights[i] * input[i]
-	}
-
-	output += n.Bias
-
-	return  output, nil
-}
 
 func mustWork(err error) {
 	if err != nil {
@@ -62,7 +16,7 @@ func main() {
 	input := []float64{1.0, 2.0, 3.0, 2.5}
 
 	// Static example
-	neuron := NewNeuron(4)
+	neuron := singleneuron.NewNeuron(4)
 	weights := []float64{0.2, 0.8, -0.5, 1.0}
 
 	neuron.Bias = 2.0
@@ -76,7 +30,7 @@ func main() {
 	fmt.Println(neuron)
 
 	// Dynamic example
-	neuron2 := NewNeuron(4)
+	neuron2 := singleneuron.NewNeuron(4)
 	neuron2.Bias = (rand.Float64() - 0.5) * 10
 	neuron2.RandomizeWeights()
 
---
+++ go/artificial-intelligence/neural-networks-from-scratch/pkgs/single-neuron/neuron.go
@@ -0,0 +1,52 @@
+package singleneuron
+
+import (
+	"errors"
+	"math/rand"
+)
+
+type NeuronFloat64 struct {
+	InputSize int
+	Bias float64
+	Weights []float64
+}
+
+func NewNeuron(numInputs int) *NeuronFloat64 {
+	return &NeuronFloat64{
+		InputSize: numInputs,
+		Bias: 0,
+		Weights: make([]float64, numInputs),
+	}
+}
+
+func (n *NeuronFloat64) SetWeights(newWeights []float64) error {
+	if len(newWeights) != n.InputSize {
+		return errors.New("size mismatch")
+	}
+
+	n.Weights = newWeights
+
+	return nil
+}
+
+func (n *NeuronFloat64) RandomizeWeights() {
+	for i := 0; i < n.InputSize; i++ {
+		n.Weights[i] = (rand.Float64() - 0.5) * 10
+	}
+}
+
+func (n *NeuronFloat64) Synapsis(input []float64) (float64, error) {
+	if len(input) != n.InputSize {
+		return 0, errors.New("size mismatch")
+	}
+
+	var output float64 = 0
+
+	for i := 0; i < len(input); i++ {
+		output += n.Weights[i] * input[i]
+	}
+
+	output += n.Bias
+
+	return  output, nil
+}
