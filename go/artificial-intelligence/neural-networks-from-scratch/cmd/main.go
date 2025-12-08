42e85bafe5f08ae9b51a5c0143c1006e9e7f79a9
Richard Almanza <al.vegari@gmail.com>
2025-12-08 00:43:31 -0500

    go/nnfs: dynamic single neuron example

--- go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
+++ go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
@@ -1,18 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+type NeuronFloat64 struct {
+	InputSize int
+	Bias float64
+	Weights []float64
+}
+
+func NewNeuron(numInputs int) *NeuronFloat64 {
+	return &NeuronFloat64{
+		InputSize: numInputs,
+		Bias: 0,
+		Weights: make([]float64, numInputs),
+	}
+}
+
+func (n *NeuronFloat64) SetWeights(newWeights []float64) error {
+	if len(newWeights) != n.InputSize {
+		return errors.New("size mismatch")
+	}
+
+	n.Weights = newWeights
+
+	return nil
+}
+
+func (n *NeuronFloat64) RandomizeWeights() {
+	for i := 0; i < n.InputSize; i++ {
+		n.Weights[i] = (rand.Float64() - 0.5) * 10
+	}
+}
+
+func (n *NeuronFloat64) Synapsis(input []float64) (float64, error) {
+	if len(input) != n.InputSize {
+		return 0, errors.New("size mismatch")
+	}
+
+	var output float64 = 0
+
+	for i := 0; i < len(input); i++ {
+		output += n.Weights[i] * input[i]
+	}
+
+	output += n.Bias
+
+	return  output, nil
+}
+
+func mustWork(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
 
 func main() {
-	var input []float32 = []float32{1.0, 2.0, 3.0, 2.5}
-	var weights []float32 = []float32{0.2, 0.8, -0.5, 1.0}
-	var bias float32 = 2.0
+	input := []float64{1.0, 2.0, 3.0, 2.5}
 
-	var output float32 = (
-		input[0] * weights[0] +
-		input[1] * weights[1] +
-		input[2] * weights[2] +
-		input[3] * weights[3] +
-		bias)
+	// Static example
+	neuron := NewNeuron(4)
+	weights := []float64{0.2, 0.8, -0.5, 1.0}
+
+	neuron.Bias = 2.0
+	err := neuron.SetWeights(weights)
+	mustWork(err)
+
+	output, err := neuron.Synapsis(input)
+	mustWork(err)
 
 	fmt.Printf("%f \n", output)
+	fmt.Println(neuron)
+
+	// Dynamic example
+	neuron2 := NewNeuron(4)
+	neuron2.Bias = (rand.Float64() - 0.5) * 10
+	neuron2.RandomizeWeights()
+
+	output2, err := neuron2.Synapsis(input)
+	mustWork(err)
+
+	fmt.Printf("%f \n", output2)
+	fmt.Println(neuron2)
 }
