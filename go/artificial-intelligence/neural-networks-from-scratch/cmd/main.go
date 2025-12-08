71f6912a7afc9cc7d9509d26708557c945eb1b70
Richard Almanza <al.vegari@gmail.com>
2025-12-07 23:49:00 -0500

    go/nffs: static single neuron example

--- go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
+++ go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello there! Let's fucking go and create Neural networks from scratch")
+	var input []float32 = []float32{1.0, 2.0, 3.0, 2.5}
+	var weights []float32 = []float32{0.2, 0.8, -0.5, 1.0}
+	var bias float32 = 2.0
+
+	var output float32 = (
+		input[0] * weights[0] +
+		input[1] * weights[1] +
+		input[2] * weights[2] +
+		input[3] * weights[3] +
+		bias)
+
+	fmt.Printf("%f \n", output)
 }
