30677c1de80d213f696488b84938fda905f45647
Richard Almanza <al.vegari@gmail.com>
2025-12-09 06:36:22 -0500

    go/nnfs: apply  softmax to the layer output's result

--- go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
+++ go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
@@ -5,31 +5,33 @@ import (
 
 	denselayer "github.com/RichardAlmanza/learning-stuff/go/artificial-intelligence/neural-networks-from-scratch/pkgs/dense-layer"
 	layerbasedneurons "github.com/RichardAlmanza/learning-stuff/go/artificial-intelligence/neural-networks-from-scratch/pkgs/layer-based-neurons"
-	"github.com/RichardAlmanza/learning-stuff/go/artificial-intelligence/neural-networks-from-scratch/pkgs/matrix"
 	"github.com/RichardAlmanza/learning-stuff/go/artificial-intelligence/neural-networks-from-scratch/pkgs/spiral"
 )
 
 func main() {
-	outputs := matrix.NewMatrixFromSlice(1, 3, []float64{4.8, 1.21, 2.385})
-	fmt.Println(outputs)
-	fmt.Println(outputs.SoftMax())
-
 	x, _ := spiral.NewSpiralData(100, 3, 0)
 
 	// original dynamic example
 
 	dense1 := denselayer.NewLayer(3, 2, denselayer.ReLuFunction)
-	outputs1 := dense1.Forward(x)
+	dense2 := denselayer.NewLayer(3, 3, denselayer.LinearFunction)
 
-	outputs1.Rows = 5 // This will limit the output to only show the first 5 rows
-	fmt.Println(outputs1)
+	outputs1 := dense1.Forward(x)
+	outputs2 := dense2.Forward(outputs1).SoftMax()
+
+	outputs2.Rows = 5 // This will limit the output to only show the first 5 rows
+	fmt.Println(outputs2)
 
 	// Dynamic example
 
 	layer1 := layerbasedneurons.NewLayerBasedNeurons(3, 2, denselayer.ReLuFunction)
 	layer1.RandomizeWeights()
+	layer2 := layerbasedneurons.NewLayerBasedNeurons(3, 3, denselayer.LinearFunction)
+	layer2.RandomizeWeights()
 
-	outputs2 := layer1.ForwardBatch(x)
-	outputs2.Rows = 5
-	fmt.Println(outputs2)
+	layer1outputs := layer1.ForwardBatch(x)
+	layer2outputs := layer2.ForwardBatch(layer1outputs).SoftMax()
+
+	layer2outputs.Rows = 5
+	fmt.Println(layer2outputs)
 }
