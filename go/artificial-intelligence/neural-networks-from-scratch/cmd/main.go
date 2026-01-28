ad7f8c1cf7d576a3d9d70b07f876dffd1f86d3d8
Richard Almanza <al.vegari@gmail.com>
2026-01-28 16:36:19 -0500

    go/nnfs: refac main.go to using the new pkgs version

--- go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
+++ go/artificial-intelligence/neural-networks-from-scratch/cmd/main.go
@@ -7,34 +7,25 @@ import (
 	denselayer "github.com/RichardAlmanza/learning-stuff/go/artificial-intelligence/neural-networks-from-scratch/pkgs/dense-layer"
 	"github.com/RichardAlmanza/learning-stuff/go/artificial-intelligence/neural-networks-from-scratch/pkgs/matrix"
 	"github.com/RichardAlmanza/learning-stuff/go/artificial-intelligence/neural-networks-from-scratch/pkgs/spiral"
+	"github.com/RichardAlmanza/learning-stuff/go/artificial-intelligence/neural-networks-from-scratch/pkgs/vector"
 )
-
-func vectorF64ToInt(vectorF []float64) []int {
-	vectorInt := make([]int, len(vectorF))
-
-	for i := 0; i < len(vectorF); i++ {
-		vectorInt[i] = int(vectorF[i])
-	}
-
-	return vectorInt
-}
 
 func main() {
 	x, y := spiral.NewSpiralData(100, 3, 0)
-	expectedLabels := matrix.NewMatrixOneHot(300, 3, vectorF64ToInt(y.Data))
+	expectedLabels := matrix.NewMatrixOneHot[float64]([]int{300, 3}, y.Data)
 
 	// original dynamic example
 	var lowestLoss float64 = math.MaxFloat64
-	bestDense1 := denselayer.NewLayer(3, 2, denselayer.ReLuFunction)
-	bestDense2 := denselayer.NewLayer(3, 3, denselayer.LinearFunction)
+	bestDense1 := denselayer.NewLayer(3, 2, denselayer.NewReLuFloat64())
+	bestDense2 := denselayer.NewLayer(3, 3, denselayer.NewLinearFloat64())
 
-	var dense2 *denselayer.Layer
-	var dense1 *denselayer.Layer
+	var dense2 *denselayer.Layer[float64]
+	var dense1 *denselayer.Layer[float64]
 
-	auxWeights1 := matrix.NewMatrix(bestDense1.Weights.Rows, bestDense1.Weights.Columns)
-	auxBiases1 := matrix.NewMatrix(1, len(bestDense1.Biases))
-	auxWeights2 := matrix.NewMatrix(bestDense2.Weights.Rows, bestDense2.Weights.Columns)
-	auxBiases2 := matrix.NewMatrix(1, len(bestDense2.Biases))
+	auxWeights1 := matrix.NewMatrix[float64](bestDense1.TWeights.Shape())
+	auxBiases1 := matrix.NewMatrix[float64]([]int{1, len(bestDense1.Biases)})
+	auxWeights2 := matrix.NewMatrix[float64](bestDense2.TWeights.Shape())
+	auxBiases2 := matrix.NewMatrix[float64]([]int{1, len(bestDense2.Biases)})
 
 	for i := 0; i < 1000; i++ {
 		dense1 = bestDense1.Copy()
@@ -45,15 +36,15 @@ func main() {
 		auxWeights2.Randomize()
 		auxBiases2.Randomize()
 
-		dense1.Weights = dense1.Weights.Add(auxWeights1.Scale(0.05))
-		dense1.Biases = matrix.SumVectors(dense1.Biases, auxBiases1.Scale(0.05).Data)
-		dense2.Weights = dense2.Weights.Add(auxWeights2.Scale(0.05))
-		dense2.Biases = matrix.SumVectors(dense2.Biases, auxBiases2.Scale(0.05).Data)
+		dense1.TWeights = dense1.TWeights.Add(auxWeights1.Scale(0.05))
+		dense1.Biases = vector.AddVectors(dense1.Biases, auxBiases1.Scale(0.05).Data)
+		dense2.TWeights = dense2.TWeights.Add(auxWeights2.Scale(0.05))
+		dense2.Biases = vector.AddVectors(dense2.Biases, auxBiases2.Scale(0.05).Data)
 
 		outputs1 := dense1.Forward(x)
-		outputs2 := dense2.Forward(outputs1).SoftMax()
+		outputs2 := matrix.SoftMax(dense2.Forward(outputs1))
 
-		loss := matrix.Avg(outputs2.CrossEntropyLossPerRow(expectedLabels))
+		loss := vector.Avg(matrix.CrossEntropyLossPerRow(outputs2, expectedLabels))
 		accuracy := outputs2.Accuracy(expectedLabels)
 
 		if loss < lowestLoss {
