d55c8dfcfa485e1afba4b49e28d90f213f90f1b8
Richard Almanza <al.vegari@gmail.com>
2026-01-29 22:00:40 -0500

    go/nnfs: refac layer and a activationFunction to separate them, now the pipeline between them wil be handle outside

--- go/artificial-intelligence/neural-networks-from-scratch/pkgs/dense-layer/activationfunction.go
+++ go/artificial-intelligence/neural-networks-from-scratch/pkgs/dense-layer/activationfunction.go
@@ -8,127 +8,118 @@ import (
 )
 
 var (
-	reLuFunctionFloat64 *ActivationFunction[float64] = nil
-	reLuFunctionFloat32 *ActivationFunction[float32] = nil
+	reLuFunctionFloat64 *ReLu[float64] = nil
+	reLuFunctionFloat32 *ReLu[float32] = nil
 
-	linearFunctionFloat64 *ActivationFunction[float64] = nil
-	linearFunctionFloat32 *ActivationFunction[float32] = nil
+	linearFunctionFloat64 *Linear[float64] = nil
+	linearFunctionFloat32 *Linear[float32] = nil
 
-	sigmoidFunctionFloat64 *ActivationFunction[float64] = nil
-	sigmoidFunctionFloat32 *ActivationFunction[float32] = nil
+	sigmoidFunctionFloat64 *Sigmoid[float64] = nil
+	sigmoidFunctionFloat32 *Sigmoid[float32] = nil
 
-	softMaxFunctionFloat64 *ActivationFunction[float64] = nil
-	softMaxFunctionFloat32 *ActivationFunction[float32] = nil
+	softMaxFunctionFloat64 *SoftMax[float64] = nil
+	softMaxFunctionFloat32 *SoftMax[float32] = nil
 
-	softMaxWCrossEntropyFunctionFloat64 *ActivationFunction[float64] = nil
-	softMaxWCrossEntropyFunctionFloat32 *ActivationFunction[float32] = nil
+	softMaxWithCrossEntropyFunctionFloat64 *SoftMaxWithCrossEntropy[float64] = nil
+	softMaxWithCrossEntropyFunctionFloat32 *SoftMaxWithCrossEntropy[float32] = nil
 )
 
 type ActivationFunction[T vector.Real] struct {
-	Function   func(*matrix.Matrix[T], *Layer[T]) *matrix.Matrix[T]
-	Derivative func(*matrix.Matrix[T], *Layer[T]) *matrix.Matrix[T]
+	Input  *matrix.Matrix[T]
+	Output *matrix.Matrix[T]
+	DInput *matrix.Matrix[T]
 }
 
-func (af *ActivationFunction[T]) Forward(input *matrix.Matrix[T], layer *Layer[T]) {
-	layer.LastInput = input.Copy()
-	layer.LastOutput = af.Function(input.Product(layer.TWeights).AddVectorPerRow(layer.Biases), layer)
-
-	// return layer.LastOutput
+type AFConstraint[T vector.Real] interface {
+	ActivationFunction[T] | ReLu[T] | Linear[T] |
+		Sigmoid[T] | SoftMax[T] | SoftMaxWithCrossEntropy[T]
 }
 
-func (af *ActivationFunction[T]) Backward(dValues *matrix.Matrix[T], layer *Layer[T]) {
-	dFunction := af.Derivative(dValues, layer).Scale(1 / float64(dValues.Shape()[0]))
+type ReLu[T vector.Real] ActivationFunction[T]
+type Linear[T vector.Real] ActivationFunction[T]
+type Sigmoid[T vector.Real] ActivationFunction[T]
+type SoftMax[T vector.Real] ActivationFunction[T]
+type SoftMaxWithCrossEntropy[T vector.Real] ActivationFunction[T]
 
-	layer.DInputs = dFunction.Product(layer.TWeights.Transpose())
-	layer.DWeights = layer.LastInput.Transpose().Product(dFunction)
-	layer.DBiases = make([]T, len(layer.Biases))
+func newReLu[T vector.Real]() *ReLu[T]       { return &ReLu[T]{} }
+func newLinear[T vector.Real]() *Linear[T]   { return &Linear[T]{} }
+func newSigmoid[T vector.Real]() *Sigmoid[T] { return &Sigmoid[T]{} }
+func newSoftMax[T vector.Real]() *SoftMax[T] { return &SoftMax[T]{} }
+func newSoftMaxWithCrossEntropy[T vector.Real]() *SoftMaxWithCrossEntropy[T] {
+	return &SoftMaxWithCrossEntropy[T]{}
+}
 
-	for i := 0; i < len(layer.DBiases); i++ {
-		layer.DBiases[i] = vector.Sum(dFunction.GetColumn(i))
-	}
+func (rl *ReLu[T]) Forward(input *matrix.Matrix[T]) {
+	rl.Input = input.Copy()
+	rl.Output = input.MapFunc(func(_ int, f T) T { return T(math.Max(0, float64(f))) })
+}
 
-	// return l.DInputs
+func (rl *ReLu[T]) Backward(dValues *matrix.Matrix[T]) {
+	rl.DInput = dValues.MapFunc(func(_ int, f T) T {
+		if f > 0 {
+			return 1
+		}
+		return 0
+	})
+}
+
+func (l *Linear[T]) Forward(input *matrix.Matrix[T]) {
+	l.Input = input.Copy()
+	l.Output = input.Copy()
+}
+
+func (l *Linear[T]) Backward(dValues *matrix.Matrix[T]) {
+	l.DInput = dValues.MapFunc(func(_ int, f T) T { return 1 })
 }
 
 func sigmoid[T vector.Real](r T) T {
 	return T(1 / (1 + math.Pow(math.E, float64(r))))
 }
 
-func newReLu[T vector.Real]() *ActivationFunction[T] {
-	return &ActivationFunction[T]{
-		Function: func(m *matrix.Matrix[T], _ *Layer[T]) *matrix.Matrix[T] {
-			return m.MapFunc(func(_ int, f T) T { return T(math.Max(0, float64(f))) })
-		},
-		Derivative: func(m *matrix.Matrix[T], _ *Layer[T]) *matrix.Matrix[T] {
-			return m.MapFunc(func(_ int, f T) T {
-				if f > 0 {
-					return 1
-				}
-				return 0
-			})
-		},
-	}
+func (s *Sigmoid[T]) Forward(input *matrix.Matrix[T]) {
+	s.Input = input.Copy()
+	s.Output = input.MapFunc(func(_ int, f T) T { return sigmoid(f) })
 }
 
-func newLinear[T vector.Real]() *ActivationFunction[T] {
-	return &ActivationFunction[T]{
-		Function: func(m *matrix.Matrix[T], _ *Layer[T]) *matrix.Matrix[T] { return m },
-		Derivative: func(m *matrix.Matrix[T], _ *Layer[T]) *matrix.Matrix[T] {
-			return m.MapFunc(func(_ int, f T) T { return 1 })
-		},
-	}
+func (s *Sigmoid[T]) Backward(dValues *matrix.Matrix[T]) {
+	s.DInput = dValues.MapFunc(func(_ int, f T) T {
+		sig := sigmoid(f)
+		return sig * (1 - sig)
+	})
 }
 
-func newSigmoid[T vector.Real]() *ActivationFunction[T] {
-	return &ActivationFunction[T]{
-		Function: func(m *matrix.Matrix[T], _ *Layer[T]) *matrix.Matrix[T] {
-			return m.MapFunc(func(_ int, f T) T { return sigmoid(f) })
-		},
-		Derivative: func(m *matrix.Matrix[T], _ *Layer[T]) *matrix.Matrix[T] {
-			return m.MapFunc(func(_ int, f T) T {
-				sig := sigmoid(f)
-				return sig * (1 - sig)
-			})
-		},
-	}
+func (s *SoftMax[T]) Forward(input *matrix.Matrix[T]) {
+	s.Input = input.Copy()
+	s.Output = matrix.SoftMax(input)
 }
 
-func newSoftMax[T vector.Real]() *ActivationFunction[T] {
-	return &ActivationFunction[T]{
-		Function: func(m *matrix.Matrix[T], _ *Layer[T]) *matrix.Matrix[T] {
-			return matrix.SoftMax(m)
-		},
-		Derivative: func(m *matrix.Matrix[T], _ *Layer[T]) *matrix.Matrix[T] {
-			// Jacobian matrices
-			newMatrices := make([]matrix.Matrix[T], m.Shape()[0])
+func (s *SoftMax[T]) Backward(dValues *matrix.Matrix[T]) {
+	// Jacobian matrices
+	newMatrices := make([]matrix.Matrix[T], dValues.Shape()[0])
 
-			for row := 0; row < m.Shape()[0]; row++ {
-				currRow := m.GetRow(row)
+	for row := 0; row < dValues.Shape()[0]; row++ {
+		currRow := dValues.GetRow(row)
 
-				diagMatrix := matrix.NewMatrixDiagonalFromSlice(currRow)
-				m1 := matrix.WrapSlice([]int{m.Shape()[1], 1}, currRow)
-				m1 = m1.Product(m1.Transpose()).Scale(-1)
+		diagMatrix := matrix.NewMatrixDiagonalFromSlice(currRow)
+		m1 := matrix.WrapSlice([]int{dValues.Shape()[1], 1}, currRow)
+		m1 = m1.Product(m1.Transpose()).Scale(-1)
 
-				newMatrices[row] = *diagMatrix.Add(m1)
-			}
-
-			return &newMatrices[0]
-		},
+		newMatrices[row] = *diagMatrix.Add(m1)
 	}
+
+	s.DInput = &newMatrices[0]
 }
 
-func newSoftMaxWCrossEntropy[T vector.Real]() *ActivationFunction[T] {
-	return &ActivationFunction[T]{
-		Function: func(m *matrix.Matrix[T], _ *Layer[T]) *matrix.Matrix[T] {
-			return matrix.SoftMax(m)
-		},
-		Derivative: func(m *matrix.Matrix[T], l *Layer[T]) *matrix.Matrix[T] {
-			return l.LastInput.DerivativeCrossEntropyLossSoftMaxPerRow(m.ToOneHot())
-		},
-	}
+func (s *SoftMaxWithCrossEntropy[T]) Forward(input *matrix.Matrix[T]) {
+	s.Input = input.Copy()
+	s.Output = matrix.SoftMax(input)
 }
 
-func singletonAF[T vector.Real](af *ActivationFunction[T], newAF func() *ActivationFunction[T]) *ActivationFunction[T] {
+func (s *SoftMaxWithCrossEntropy[T]) Backward(dValues *matrix.Matrix[T], targets []int) {
+	s.DInput = dValues.DerivativeCrossEntropyLossSoftMaxPerRow(targets)
+}
+
+func singletonAF[T vector.Real, AF AFConstraint[T]](af *AF, newAF func() *AF) *AF {
 	if af == nil {
 		af = newAF()
 	}
@@ -138,41 +129,41 @@ func singletonAF[T vector.Real](af *ActivationFunction[T], newAF func() *Activat
 
 // Exported functions for creating ActivationFunctions.
 
-func NewReLuFloat64() *ActivationFunction[float64] {
+func NewReLuFloat64() *ReLu[float64] {
 	return singletonAF(reLuFunctionFloat64, newReLu)
 }
 
-func NewReLuFloat32() *ActivationFunction[float32] {
+func NewReLuFloat32() *ReLu[float32] {
 	return singletonAF(reLuFunctionFloat32, newReLu)
 }
 
-func NewLinearFloat64() *ActivationFunction[float64] {
+func NewLinearFloat64() *Linear[float64] {
 	return singletonAF(linearFunctionFloat64, newLinear)
 }
 
-func NewLinearFloat32() *ActivationFunction[float32] {
+func NewLinearFloat32() *Linear[float32] {
 	return singletonAF(linearFunctionFloat32, newLinear)
 }
-func NewSigmoidFloat64() *ActivationFunction[float64] {
+func NewSigmoidFloat64() *Sigmoid[float64] {
 	return singletonAF(sigmoidFunctionFloat64, newSigmoid)
 }
 
-func NewSigmoidFloat32() *ActivationFunction[float32] {
+func NewSigmoidFloat32() *Sigmoid[float32] {
 	return singletonAF(sigmoidFunctionFloat32, newSigmoid)
 }
 
-func NewSoftMaxFloat64() *ActivationFunction[float64] {
+func NewSoftMaxFloat64() *SoftMax[float64] {
 	return singletonAF(softMaxFunctionFloat64, newSoftMax)
 }
 
-func NewSoftMaxFloat32() *ActivationFunction[float32] {
+func NewSoftMaxFloat32() *SoftMax[float32] {
 	return singletonAF(softMaxFunctionFloat32, newSoftMax)
 }
 
-func NewSoftMaxWCrossEntropyFloat64() *ActivationFunction[float64] {
-	return singletonAF(softMaxWCrossEntropyFunctionFloat64, newSoftMaxWCrossEntropy)
+func NewSoftMaxWCrossEntropyFloat64() *SoftMaxWithCrossEntropy[float64] {
+	return singletonAF(softMaxWithCrossEntropyFunctionFloat64, newSoftMaxWithCrossEntropy)
 }
 
-func NewSoftMaxWCrossEntropyFloat32() *ActivationFunction[float32] {
-	return singletonAF(softMaxWCrossEntropyFunctionFloat32, newSoftMaxWCrossEntropy)
+func NewSoftMaxWCrossEntropyFloat32() *SoftMaxWithCrossEntropy[float32] {
+	return singletonAF(softMaxWithCrossEntropyFunctionFloat32, newSoftMaxWithCrossEntropy)
 }
--- go/artificial-intelligence/neural-networks-from-scratch/pkgs/dense-layer/layer.go
+++ go/artificial-intelligence/neural-networks-from-scratch/pkgs/dense-layer/layer.go
@@ -6,55 +6,57 @@ import (
 )
 
 type Layer[T vector.Real] struct {
-	TWeights           *matrix.Matrix[T]
-	Biases             []T
-	LastInput          *matrix.Matrix[T]
-	LastOutput         *matrix.Matrix[T]
-	DInputs            *matrix.Matrix[T]
-	DWeights           *matrix.Matrix[T]
-	DBiases            []T
-	ActivationFunction *ActivationFunction[T]
+	TWeights *matrix.Matrix[T]
+	Biases   []T
+	Input    *matrix.Matrix[T]
+	Output   *matrix.Matrix[T]
+	DInputs  *matrix.Matrix[T]
+	DWeights *matrix.Matrix[T]
+	DBiases  []T
 }
 
-func NewLayer[T vector.Real](nNeurons, inputSize int, function *ActivationFunction[T]) *Layer[T] {
+func NewLayer[T vector.Real](nNeurons, inputSize int) *Layer[T] {
 	shape := []int{inputSize, nNeurons}
-	newTWeights := matrix.NewMatrixRand[T](shape).Scale(0.01)
 
 	return &Layer[T]{
-		TWeights:           newTWeights,
-		Biases:             make([]T, nNeurons),
-		ActivationFunction: function,
+		TWeights: matrix.NewMatrixRand[T](shape).Scale(0.01),
+		Biases:   make([]T, nNeurons),
 	}
 }
 
-func NewLayerFrom[T vector.Real](tWeights *matrix.Matrix[T], biases []T, function *ActivationFunction[T]) *Layer[T] {
+func NewLayerFrom[T vector.Real](tWeights *matrix.Matrix[T], biases []T) *Layer[T] {
 	if len(biases) != tWeights.Shape()[1] {
 		panic("Size mismatch")
 	}
 
 	return &Layer[T]{
-		TWeights:           tWeights,
-		Biases:             biases,
-		ActivationFunction: function,
+		TWeights: tWeights.Copy(),
+		Biases:   vector.NewCopy(biases),
 	}
 }
 
 func (l *Layer[T]) Forward(input *matrix.Matrix[T]) *matrix.Matrix[T] {
-	l.ActivationFunction.Forward(input, l)
+	l.Input = input.Copy()
+	l.Output = input.Product(l.TWeights).AddVectorPerRow(l.Biases)
 
-	return l.LastOutput
+	return l.Output
 }
 
 func (l *Layer[T]) Backward(dValues *matrix.Matrix[T]) *matrix.Matrix[T] {
-	l.ActivationFunction.Backward(dValues, l)
+	l.DInputs = dValues.Product(l.TWeights.Transpose())
+	l.DWeights = l.Input.Transpose().Product(dValues)
+	l.DBiases = make([]T, len(l.Biases))
+
+	for i := 0; i < len(l.Biases); i++ {
+		l.DBiases[i] = vector.Sum(dValues.GetColumn(i))
+	}
 
 	return l.DInputs
 }
 
 func (l *Layer[T]) Copy() *Layer[T] {
 	return &Layer[T]{
-		TWeights:           l.TWeights.Copy(),
-		Biases:             vector.NewCopy(l.Biases),
-		ActivationFunction: l.ActivationFunction,
+		TWeights: l.TWeights.Copy(),
+		Biases:   vector.NewCopy(l.Biases),
 	}
 }
