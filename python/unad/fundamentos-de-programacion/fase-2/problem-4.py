cf720c53a69fa933f346b74b87ab28766032642f
Richard Almanza <al.vegari@gmail.com>
2026-02-26 01:11:07 -0500

    python/.../fase-2: lint problem-4.py

--- python/unad/fundamentos-de-programacion/fase-2/problem-4.py
+++ python/unad/fundamentos-de-programacion/fase-2/problem-4.py
@@ -10,6 +10,11 @@
 # - Si el código es inválido, mostrar error y finalizar.
 # - Calcular y mostrar el valor final según la categoría.
 
+# Nombre:           Richard A. Vega Almanza
+# Grupo:            213022_154
+# Programa:         Ingenieria Electronica
+# Codigo Fuente:    Autoria Propia
+
 IVA = 0.19
 EXTRA = 0.05
 
@@ -17,27 +22,29 @@ base_price = float(input("Ingrese el valor del producto base: "))
 total = base_price
 
 if base_price > 0:
-    product_category = int(input("""Ingrese la opcion correspondiente (numero entero) a la categoria del producto:
+    product_category = int(
+        input("""Ingrese la opcion correspondiente (numero entero) a la categoria del producto:
 1. Basico
 2. Estandar
 3. Lujo
-"""))
-    
+""")
+    )
+
     if 1 <= product_category and product_category <= 3:
         if product_category == 1:
             print("No paga IVA")
         elif product_category == 2:
-            print(f"Paga el IVA de {IVA*100: .0f}%")
-            total += base_price * IVA
+            print(f"Paga el IVA de {IVA * 100: .0f}%")
+            total *= 1 + IVA
         elif product_category == 3:
-            print(f"Paga el IVA de {IVA*100: .0f}% + recargo del {EXTRA*100: .0f}%")
-            total += base_price * (IVA + EXTRA)
+            print(f"Paga el IVA de {IVA * 100: .0f}% + recargo del {EXTRA * 100: .0f}%")
+            total *= 1 + IVA + EXTRA
         else:
             print("Este mensaje nunca debio aparecer!")
-        
+
         print(f"El total a pagar es {total}")
     else:
         print("Categoria invalida")
-    
+
 else:
     print("Valor invalido")
