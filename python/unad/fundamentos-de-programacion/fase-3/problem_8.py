6569af751d6790e92de7c75b768c2e2a5ca3d754
Richard Almanza <al.vegari@gmail.com>
2026-03-29 23:55:23 -0500

    python/.../fase-3: add problem 8 definition from stage 3

---
+++ python/unad/fundamentos-de-programacion/fase-3/problem_8.py
@@ -0,0 +1,50 @@
+# La familia de Pepito ha celebrado sus cumpleaños desde el
+# primer año de vida, y en el año 2026 cumplirá 6 años, por lo que
+# nuevamente organizarán una fiesta con familiares y amigos. Los padres
+# del niño son muy cuidadosos con sus finanzas y llevan un control
+# detallado de sus gastos.
+# El año anterior tuvieron un inconveniente con el presupuesto destinado
+# a la compra de helados, ya que no lograron llevar un registro exacto.
+# Para evitar que esto vuelva a ocurrir, le han solicitado desarrollar un
+# programa que permita gestionar la compra y el control del dinero
+# disponible.
+#
+# Requisitos del programa
+# 1. Entrada inicial:
+# - Solicitar la cantidad de dinero disponible para realizar la
+# compra de helados.
+#
+# 2. Proceso de compra:
+# - El usuario podrá comprar la cantidad de helados que desee,
+# siempre que el dinero alcance.
+# - Cada helado se selecciona según la siguiente Información:
+# Tipo      Descripción         Valor
+# 1         vaso de Helado      $2.500
+# 2         choco cono          $2.000
+# 3         paleta Drácula      $3.800
+# 4         polet               $4.500
+# 5         platillo            $1.800
+# - Los valores deben almacenarse en un arreglo unidimensional,
+# donde la posición 0 corresponde al primer tipo de helado, la
+# posición 1 al segundo, y así sucesivamente:
+# Posición  0           1           2           3           4
+#           $2.500      $2.000      $3.800      $4.500      $1.800
+# - Después de cada compra, el programa debe mostrar el dinero
+# restante para que el usuario tenga control del presupuesto.
+#
+# 3. Condiciones para finalizar el ciclo de compra:
+# - Cuando el dinero restante no alcance para comprar ningún tipo
+# de helado (el programa debe mostrar un mensaje indicando esta
+# situación).
+# - O cuando el usuario decida no continuar comprando.
+# Al terminar el proceso, el programa debe mostrar:
+# • Cantidad total de helados comprados.
+# • Cantidad de helados por cada tipo.
+# • Valor total gastado.
+# • Valor promedio por helado.
+# • Dinero sobrante.
+
+# Nombre:           Richard A. Vega Almanza
+# Grupo:            213022_154
+# Programa:         Ingenieria Electronica
+# Codigo Fuente:    Autoria Propia
