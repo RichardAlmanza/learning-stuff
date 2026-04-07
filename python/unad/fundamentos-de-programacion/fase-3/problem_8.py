e7f217cf983095c114a210420830f5b2cc2db41b
Richard Almanza <al.vegari@gmail.com>
2026-04-07 00:00:01 -0500

    python/.../fase-3: add solution for problem 8 from stage 3

--- python/unad/fundamentos-de-programacion/fase-3/problem_8.py
+++ python/unad/fundamentos-de-programacion/fase-3/problem_8.py
@@ -1,4 +1,4 @@
-# La familia de Pepito ha celebrado sus cumpleaños desde el
+# Problema 8: La familia de Pepito ha celebrado sus cumpleaños desde el
 # primer año de vida, y en el año 2026 cumplirá 6 años, por lo que
 # nuevamente organizarán una fiesta con familiares y amigos. Los padres
 # del niño son muy cuidadosos con sus finanzas y llevan un control
@@ -48,3 +48,156 @@
 # Grupo:            213022_154
 # Programa:         Ingenieria Electronica
 # Codigo Fuente:    Autoria Propia
+
+from problem_3 import ask, try_int, natural_validator
+
+
+def format_price(value: int|float) -> str:
+    """Format number to currency"""
+    return f"${value:,.0f}".replace(',','.')
+
+
+def format_shopping_options(
+        name_list: list[str],
+        price_list: list[float|int],
+        headers: list[str]=None) -> str:
+    """Format a list for shopping options based on item names and their prices"""
+
+    if not name_list:
+        return ""
+
+    output = []
+
+    max_length_in_names = len(max(name_list))
+
+    if headers:
+        padding = (max_length_in_names - len(headers[1])) * " "
+        output.append(f"{headers[0]}\t{headers[1]}{padding}\t\t{headers[2]}")
+
+    for i, (name, price) in enumerate(zip(name_list, price_list)):
+        padding = (max_length_in_names - len(name)) * " "
+        output.append(f"{i + 1}.\t{name}{padding}\t\t{format_price(price)}")
+
+    return "\n".join(output)
+
+
+def filter_available_shopping_options(
+        name_list: list[str],
+        price_list: list[float|int],
+        budget: int|float):
+    """Filter the shopping options based on budget"""
+
+    mapped = []
+    new_name_list = []
+    new_price_list = []
+
+    for i, (name, price) in enumerate(zip(name_list, price_list)):
+        if price <= budget:
+            mapped.append(i)
+            new_name_list.append(name)
+            new_price_list.append(price)
+
+    return [mapped, new_name_list, new_price_list]
+
+
+def option_validator_creator(min_value: int, max_value: int):
+    """
+    Return a validator function for the user input,
+    to validate if the selected option is inside of the range described
+    """
+
+    if max_value < min_value:
+        max_value, min_value = min_value, max_value
+
+    def option_validator(user_input):
+        option, ok = try_int(user_input)
+
+        if min_value <= option and option <= max_value and ok:
+            return [option, True]
+
+        return [0, False]
+
+    return option_validator
+
+
+def calculate_bill(prices: list[int|float], quantities: list[int]):
+    """Sum total cost of the items"""
+
+    total = 0
+    for price, quantity in zip(prices, quantities):
+        total += price * quantity
+
+    return total
+
+
+if __name__ == "__main__":
+    ORIGINAL_BUDGET = ask(
+        "Por favor ingrese el dinero disponible para comprar? ",
+        "Ingrese un numero entero positivo, sin centavos, ni simbolos o separadores",
+        natural_validator
+    )
+    budget = ORIGINAL_BUDGET
+
+    fields = ["Tipo", "Descripcion", "Precio"]
+    ice_cream_available = ["Vaso de helado", "Chococono", "Paleta Dracula", "Polet", "Platillo"]
+    ice_cream_prices = [2500, 2000, 3800, 4500, 1800]
+    quantities = [ 0 for _ in ice_cream_available ]
+
+    # Buy process
+    while True:
+        # Calculate current budget
+        budget = ORIGINAL_BUDGET - calculate_bill(ice_cream_prices, quantities)
+        print(f"Dinero disponible {format_price(budget)}")
+
+        # Generate item list for items available
+        mapped, filtered_ic, filtered_prices = filter_available_shopping_options(
+            ice_cream_available, ice_cream_prices, budget)
+
+        if not filtered_ic:
+            print("No tiene suficiente dinero disponible para seguir comprando")
+            break
+
+        # Ask for the user to select a item to buy
+        MAX_OPTION = len(filtered_ic) + 1
+        shopping_options = format_shopping_options(filtered_ic, filtered_prices, fields)
+        selected_option = ask(
+            f"{shopping_options}\n{MAX_OPTION}.\tFinalizar la compra\n",
+            "Por favor ingrese el numero relacionado con la opcion.",
+            option_validator_creator(1, MAX_OPTION)
+        )
+
+        # Breaks the loop based on user selection
+        if selected_option == MAX_OPTION:
+            break
+
+        # Get original list index
+        index = mapped[selected_option - 1]
+
+        # Buy multiple ice creams
+        MAX_ITEMS = int(budget / ice_cream_prices[index])
+        SELECTED_ICE_CREAM = ice_cream_available[index]
+        quantity = ask(
+            f"Cuantos {SELECTED_ICE_CREAM} desea comprar? con el dinero actual puede un maximo de {MAX_ITEMS}: ",
+            f"Por favor ingrese numeros enteros desde 0 (para cancelar la compra) hasta {MAX_ITEMS}",
+            option_validator_creator(0, MAX_ITEMS)
+        )
+
+        quantities[index] += quantity
+
+    # Resume
+    # • Cantidad total de helados comprados.
+    total = sum(quantities)
+    print(f"Total de helados comprados: {total}")
+    # • Cantidad de helados por cada tipo.
+    for ice_cream, quantity in zip(ice_cream_available, quantities):
+        if quantity > 0:
+            print(f"Se compraron {quantity} helados de {ice_cream}")
+    # • Valor total gastado.
+    bill = calculate_bill(ice_cream_prices, quantities)
+    print(f"Se gasto en total: {format_price(bill)}")
+    # • Valor promedio por helado.
+    avg = bill / (total if total != 0 else 1)
+    print(f"El valor promedio por helado es de {format_price(avg)}")
+    # • Dinero sobrante.
+    budget = ORIGINAL_BUDGET - bill
+    print(f"El dinero sobrante es de {format_price(budget)}")
